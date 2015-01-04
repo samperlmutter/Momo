28083a52751b86de7e31a09af3b34400b3f24c51
Sam Perlmutter <sam.perlmutter227@gmail.com>
2015-01-04 17:13:15 -0500

    Fixed climber code

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -4,14 +4,13 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.templates;
-
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -20,19 +19,17 @@ import edu.wpi.first.wpilibj.Timer;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-
-
 public class RobotTemplate extends SimpleRobot {
-    /**
-     * This function is called once each time the robot enters autonomous mode.
-     */
-    
-    Joystick leftJoy = new Joystick(1);
-    Joystick rightJoy = new Joystick(2);
-    RobotDrive robotDrive =  new RobotDrive(4, 3, 2, 1);
-    
+
+    Joystick leftJoyStick = new Joystick(1);
+    Joystick rightJoyStick = new Joystick(2);
+    RobotDrive robotDrive = new RobotDrive(4, 3, 2, 1);
+    DoubleSolenoid climberPiston = new DoubleSolenoid(3, 4);
+    Relay compressorSpike = new Relay(1);
+    DigitalInput digitalCompresser = new DigitalInput(1);
+
     public void autonomous() {
-        
+
     }
 
     /**
@@ -40,19 +37,34 @@ public class RobotTemplate extends SimpleRobot {
      */
     public void operatorControl() {
         robotDrive.setSafetyEnabled(true);
-        
-        while (isOperatorControl() && isEnabled())
-        {
-            robotDrive.tankDrive(leftJoy, rightJoy);
+
+        while (isOperatorControl() && isEnabled()) {
+            robotDrive.tankDrive(-leftJoyStick.getY(), -rightJoyStick.getY());
+            if (rightJoyStick.getRawButton(6)) {
+                climberPiston.set(DoubleSolenoid.Value.kForward);
+            }
+            if (rightJoyStick.getRawButton(7)) {
+                climberPiston.set(DoubleSolenoid.Value.kReverse);
+            }
+            if (rightJoyStick.getRawButton(8)) {
+                climberPiston.set(DoubleSolenoid.Value.kOff);
+            }
+            if (!digitalCompresser.get()) {
+                compressorSpike.set(Relay.Value.kForward);
+            } else {
+                compressorSpike.set(Relay.Value.kOff);
+                
+            }
             Timer.delay(0.01);
+
         }
-        
+
     }
-    
+
     /**
      * This function is called once each time the robot enters test mode.
      */
     public void test() {
-    
+
     }
 }
