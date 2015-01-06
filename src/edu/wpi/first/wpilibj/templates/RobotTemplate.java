2f6c2ba039cac0be71b355f304864b2a667d463a
Sam Perlmutter <sam.perlmutter227@gmail.com>
2015-01-05 21:02:28 -0500

    Added magazine code

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -21,12 +21,13 @@ import edu.wpi.first.wpilibj.*;
  */
 public class RobotTemplate extends SimpleRobot {
 
-    Joystick leftJoyStick = new Joystick(1);
-    Joystick rightJoyStick = new Joystick(2);
+    Joystick Joystick1 = new Joystick(1);
+    Joystick JoyStick2 = new Joystick(2);
     RobotDrive robotDrive = new RobotDrive(4, 3, 2, 1);
     DoubleSolenoid climberPiston = new DoubleSolenoid(3, 4);
     Relay compressorSpike = new Relay(1);
     DigitalInput digitalCompresser = new DigitalInput(1);
+    DoubleSolenoid magazine = new DoubleSolenoid(1, 2);
 
     public void autonomous() {
 
@@ -39,14 +40,14 @@ public class RobotTemplate extends SimpleRobot {
         robotDrive.setSafetyEnabled(true);
 
         while (isOperatorControl() && isEnabled()) {
-            robotDrive.tankDrive(-leftJoyStick.getY(), -rightJoyStick.getY());
-            if (rightJoyStick.getRawButton(6)) {
+            robotDrive.tankDrive(-Joystick1.getY(), -JoyStick2.getY());
+            if (JoyStick2.getRawButton(6)) {
                 climberPiston.set(DoubleSolenoid.Value.kForward);
             }
-            if (rightJoyStick.getRawButton(7)) {
+            if (JoyStick2.getRawButton(7)) {
                 climberPiston.set(DoubleSolenoid.Value.kReverse);
             }
-            if (rightJoyStick.getRawButton(8)) {
+            if (JoyStick2.getRawButton(8)) {
                 climberPiston.set(DoubleSolenoid.Value.kOff);
             }
             if (!digitalCompresser.get()) {
@@ -54,6 +55,14 @@ public class RobotTemplate extends SimpleRobot {
             } else {
                 compressorSpike.set(Relay.Value.kOff);
                 
+            }
+            if (JoyStick2.getRawButton(11))
+            {
+                magazine.set(DoubleSolenoid.Value.kForward);
+            }
+            if (JoyStick2.getRawButton(10))
+            {
+                magazine.set(DoubleSolenoid.Value.kReverse);
             }
             Timer.delay(0.01);
 
